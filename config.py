5dd5c3ef0dfa55872685cee98d691ece39c67a62
rbn42 <bl100@students.waikato.ac.nz>
2016-04-24 14:37:43 +1200

    comment

--- config.py
+++ config.py
@@ -6,11 +6,19 @@ RightPadding = BottomPadding
 NavigateAcrossWorkspaces = True  # availabe in Unity7
 TempFile = "/dev/shm/.stiler_db"
 LockFile = "/dev/shm/.stiler.lock"
+
+'''
+This is the congiguration that works for unity7. 
+If you are using a different Desktop Environment, 
+close all windows and execute "wmctrl -lG" to 
+find out all the applications need to exclude.
+'''
+
 EXCLUDE_APPLICATIONS = ['<unknown>', 'x-nautilus-desktop', 'unity-launcher',
-                        'unity-panel', 'Hud',
-                        'unity-dash', 'Desktop',
+                        'unity-panel', 'Hud', 'unity-dash', 'Desktop',
                         'Docky',
                         'screenkey', 'XdndCollectionWindowImp']
+
 UNRESIZABLE_APPLICATIONS = ['Screenkey']
 RESIZE_STEP = 50
 MOVE_STEP = 50
@@ -18,4 +26,6 @@ MIN_WINDOW_WIDTH = 50
 MIN_WINDOW_HEIGHT = 50
 
 #NOFRAME_WMCLASS = ['Wine']
+
+# In i3-wm's window tree, only one child of a node is allowed to split.
 MAX_KD_TREE_BRANCH = 1
